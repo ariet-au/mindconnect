ad028c5ee00b37f0281d75731407d67a0ccc8f87
Ariet Dzhumashev <aktonov.nl@gmail.com>
2025-06-05 08:25:00 +1000

    changed email config aagain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,10 @@ Rails.application.configure do
   # }
     # Set localhost to be used by links generated in mailer templates.
   # Set localhost to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+config.action_mailer.default_url_options = {
+  host: "mindconnect-9x2c.onrender.com",
+  protocol: "https"
+}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
