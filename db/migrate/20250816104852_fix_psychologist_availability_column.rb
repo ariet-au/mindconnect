61bec0066986ded4ffdd865ef0a8b106ec44be3c
Ariet Dzhumashev <aktonov.nl@gmail.com>
2025-08-16 20:51:07 +1000

    Rollback broken psychologist_availabilities column types

---
+++ db/migrate/20250816104852_fix_psychologist_availability_column.rb
@@ -0,0 +1,20 @@
+class FixPsychologistAvailabilityColumn  < ActiveRecord::Migration[8.0]
+  def up  
+    execute <<-SQL
+      ALTER TABLE psychologist_availabilities
+      ALTER COLUMN start_time_of_day TYPE timestamp without time zone
+      USING ('2000-01-01 ' || start_time_of_day)::timestamp;
+    SQL
+
+    execute <<-SQL
+      ALTER TABLE psychologist_availabilities
+      ALTER COLUMN end_time_of_day TYPE timestamp without time zone
+      USING ('2000-01-01 ' || end_time_of_day)::timestamp;
+    SQL
+  end
+
+  def down
+    change_column :psychologist_availabilities, :start_time_of_day, :time
+    change_column :psychologist_availabilities, :end_time_of_day, :time
+  end
+end
