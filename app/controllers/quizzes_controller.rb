8d094e05abdc9273305edfc27f374e607b826178
Ariet Dzhumashev <aktonov.nl@gmail.com>
2025-09-16 20:47:54 +1000

    changes to quiz controller before action

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,5 +1,5 @@
 class QuizzesController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :take, :submit, :result]
   before_action :set_quiz, only: [:show, :edit, :update, :destroy]
 
   # GET /quizzes or /quizzes.json
