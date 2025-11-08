5b52d051e288a4c2aac3b5fae5a593df38884dcb
Ariet Dzhumashev <aktonov.nl@gmail.com>
2025-11-08 17:56:49 +1100

    changes to about

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,8 @@ class PagesController < ApplicationController
   .order("count_all DESC")
   .count
 
-  @psychologist_profiles = PsychologistProfile.filled.not_hidden.limit(5)
+  @psychologist_profiles = PsychologistProfile.confirmed.filled.active.not_hidden.order(updated_at: :desc)
+
   @quizzes = Quiz.limit(4)
     # Count those who offer online sessions
     @online_count = PsychologistProfile.where(online: true).count
