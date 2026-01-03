f9940106ca46c5d393bc72abd2a16efdb6eff138
Ariet Dzhumashev <aktonov.nl@gmail.com>
2026-01-03 16:12:55 +1100

    changes to amplitude

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,3 +9,9 @@ import "@rails/actiontext"
 
 
 
+document.addEventListener("turbo:load", function() {
+  if (window.amplitude) {
+    // This tells Amplitude "I moved to a new page" even without a refresh
+    window.amplitude.track('Page View', { url: window.location.pathname });
+  }
+});
