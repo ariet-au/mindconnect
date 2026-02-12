fec2117630cb94fe33ed51ebcbc188c202e3ce29
Ariet Dzhumashev <aktonov.nl@gmail.com>
2026-02-13 07:15:38 +1100

    changes to files

---
+++ app/services/profile_matcher.rb
@@ -0,0 +1,22 @@
+# app/services/psychologist_matcher.rb
+class ProfileMatcher
+  def self.match_for_labels(predicted_labels)
+    return [] if predicted_labels.blank?
+
+    labels_array = predicted_labels.map { |l| l[:label] }
+
+    matching_psychologists = PsychologistProfile
+      .joins(psychologist_issues: :issue)
+      .merge(PsychologistProfile.with_profile_img)
+      .merge(PsychologistProfile.not_hidden)
+      .where(issues: { category: labels_array })
+      .distinct
+      .includes(psychologist_issues: :issue)
+
+    matching_psychologists.map do |p|
+      covered_categories = p.psychologist_issues.map { |pi| pi.issue.category } & labels_array
+      score = covered_categories.sum { |cat| predicted_labels.find { |l| l[:label] == cat }[:score] }
+      { psychologist: p, score: score, matched_categories: covered_categories }
+    end.sort_by { |h| -h[:score] }
+  end
+end
